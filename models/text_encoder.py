4e7468d1f4b581f307591fe8ba8315658c7725ea
huuthientran <huuthien013@gmail.com>
2024-05-13 18:40:10 +0700

    modify to add clip model

--- models/text_encoder.py
+++ models/text_encoder.py
@@ -1,5 +1,4 @@
 # text encoder
-from transformers import RobertaModel, RobertaTokenizerFast
 import os 
 
 def build_roberta(config):
@@ -8,6 +7,7 @@ def build_roberta(config):
         tokenizer: RobertaTokenizerFast
         text_encoder: RobertaModel
     """
+    from transformers import RobertaModel, RobertaTokenizerFast
     hf_local_path = config['HF_LOCAL_PATH']
     model_name = config['MODEL_NAME']
     if not os.path.exists(hf_local_path):
@@ -20,21 +20,46 @@ def build_roberta(config):
         tokenizer = RobertaTokenizerFast.from_pretrained(hf_local_path)
         text_encoder = RobertaModel.from_pretrained(hf_local_path)
     
-    return tokenizer, text_encoder
-
-
-def build(config):
-    options = {
-        'roberta-base': 'roberta-base',
-    }
-    model_name = options[config['MODEL_NAME']]
-    assert model_name is not None, f"Model {config['MODEL_NAME']} not supported"
-
-    if model_name == 'roberta-base':
-        tokenizer, text_encoder = build_roberta(config)
-
     if config['FREEZE_TEXT_ENCODER']:
         for param in text_encoder.parameters():
             param.requires_grad = False
 
     return tokenizer, text_encoder
+
+
+def build_clip(config):
+    """
+    Returns:
+        tokenizer: CLIPProcessor
+        text_encoder: CLIPModel
+    """
+    import clip
+    import torch
+    device = config['DEVICE']
+    model_name = config['MODEL_NAME']
+    model, _ = clip.load(model_name, device=torch.device(device))
+
+    # Freeze text encoder
+    if config['FREEZE_TEXT_ENCODER']:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    return model # used via `model.encode_text(clip.tokenize(["a list of strings"]))`
+
+
+def build(config):
+    options = {
+        'roberta-base': 'roberta-base',
+        'clip-rn': 'RN50',
+        'clip-vit': 'ViT-B/32',
+    }
+    model_name = options[config['MODEL_NAME']]
+    assert model_name is not None, f"Model {config['MODEL_NAME']} not supported"
+
+    if model_name == 'roberta-base':
+        return build_roberta(config)
+    
+    if model_name in ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14', 'ViT-L/14@336px']:
+        return build_clip(config)
+    
+    raise NotImplementedError(f"Model {model_name} not implemented")
