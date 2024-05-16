5f6e281b30081a66527d6d2259c6648a948530ce
huuthientran <huuthien013@gmail.com>
2024-05-16 17:47:01 +0700

    necessary change to align w updated MOT17 dataset
    IMPORTANT: following modifications on the dataset involved in the Prompt-tracking model should contain `sentences` like this so that it could be more easily to maintain.

--- data/transforms.py
+++ data/transforms.py
@@ -165,6 +165,9 @@ class MultiRandomCrop:
 
                 for field in ["labels", "ids", "boxes", "areas"]:
                     info[field] = info[field][keep_idxs]
+                
+                # (new)
+                info["sentences"] = [info["sentences"][i] for i, keep in enumerate(keep_idxs) if keep]
             return cropped_img, info
 
         return zip(*[crop(img, info, ijhw=crop_ijhw) for img, info in zip(imgs, infos)])
